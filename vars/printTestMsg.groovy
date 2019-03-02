759bc0e699ae05f714f4b26b9eb24c4411b587a6
canokcelik <canokcelik@hotmail.com>
2019-03-02 23:27:45 +0300

    new two func added

--- vars/printTestMsg.groovy
+++ vars/printTestMsg.groovy
@@ -1,3 +1,26 @@
 def printTestMsg(String input) {
     return input    
 }
+
+def getConfigMapParameters(String ns, String cm) {
+        def configmapsJson=("oc get cm "+cm+" -o json "+"-n "+ns).execute().text
+        
+        def jsonSlurper = new JsonSlurper()
+        def object = jsonSlurper.parseText(configmapsJson)
+        def keyList=[]
+        
+        object.data.each {
+            keyList.add(it.key)
+        }
+        
+        return keyList
+}
+
+def getConfigMapsList(String ns) {
+        def configmapsJson=("oc get cm -o json "+"-n "+ns).execute().text
+        
+        def jsonSlurper = new JsonSlurper()
+        def object = jsonSlurper.parseText(configmapsJson)
+        
+        return object.items.metadata.name
+}
